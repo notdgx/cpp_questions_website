289c8ff32893c675d54e4eb91b966b47f615b49c
notdgx <219498562+notdgx@users.noreply.github.com>
2026-04-07 21:35:07 +0530

    Updated for github pages deployement

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: '/cpp_questions_website/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
